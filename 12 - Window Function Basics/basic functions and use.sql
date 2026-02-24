a41041a02201a4a6a1363f7452e00d50ea26257d
tye-codes <tye.routh@gmail.com>
2026-02-24 22:51:17 +1000

    Update basic functions and use.sql

--- 12 - Window Function Basics/basic functions and use.sql
+++ 12 - Window Function Basics/basic functions and use.sql
@@ -1,7 +1,9 @@
 SELECT
+customer_id, 
 COUNT(*) as total_nr_orders,
 SUM(sales) AS total_sales,
 AVG(sales) AS avg_sales,
 MAX(sales) AS highest_sales,
 MIN(sales) AS lowest_sales
 FROM orders
+GROUP BY customer_id
